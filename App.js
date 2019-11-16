0ce3f682e948b2910d07199260502f99de980e85
Chris_Reams <chrisreams91@gmail.com>
2019-11-16 15:49:07 -0600

    set up basic tabbar scaffolding

--- App.js
+++ App.js
@@ -17,90 +17,36 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import TVTabBar from './TVTabBar';
+import VictoryDemo from './VictoryDemo';
+
+const tabs = [
+  {
+    key: 'home',
+    name: 'home',
+    value: (
+      <View>
+        <Text style={{fontSize: 300}}>TEST</Text>
+      </View>
+    ),
+  },
+  {
+    key: 'Victory',
+    name: 'Victory',
+    value: <VictoryDemo />,
+  },
+];
+
 const App: () => React$Node = () => {
   return (
-    <>
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <TVTabBar
+      barColor="#00a1e0"
+      textColor="white"
+      selectedTextColor="red"
+      tabs={tabs}
+      defaultTabKey="home"
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ VictoryDemo.js
@@ -0,0 +1,162 @@
+/* @flow */
+import {random, range} from 'lodash';
+import React, {Component} from 'react';
+import {Image, View, Text} from 'react-native';
+
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryScatter,
+  VictoryTheme,
+  VictoryPie,
+} from 'victory-native';
+
+class VictoryDemo extends Component<
+  {},
+  {
+    randomData: Object,
+  },
+> {
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      randomData: this.generateRandomData(),
+    };
+  }
+
+  componentDidMount() {
+    setInterval(this.updateDemoData.bind(this), 3000);
+  }
+
+  getStyles() {
+    const colors = ['red', 'orange', 'magenta', 'gold', 'blue', 'purple'];
+    return {
+      stroke: colors[random(0, 5)],
+      strokeWidth: random(1, 5),
+    };
+  }
+
+  getTransitionData() {
+    const n = random(4, 10);
+    return range(n).map(i => ({
+      x: i,
+      y: random(2, 10),
+    }));
+  }
+
+  updateDemoData() {
+    this.setState({
+      randomData: this.generateRandomData(),
+    });
+  }
+
+  generateRandomData(points: number = 6) {
+    return range(1, points + 1).map(i => ({x: i, y: i + random(-1, 2)}));
+  }
+
+  render() {
+    return (
+      <View style={{alignItems: 'center', margin: 20, marginTop: 150}}>
+        <Text style={{marginTop: 50, fontSize: 100}}>FLEX METRICS !!!!</Text>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{alignItems: 'center'}}>
+            <VictoryChart width={500} theme={VictoryTheme.material}>
+              <VictoryBar
+                width={500}
+                style={{
+                  labels: {fontSize: 30},
+                  data: {fill: 'tomato', opacity: 0.5},
+                }}
+                data={[
+                  {x: 15, y: 20, label: 1, fill: 'red'},
+                  {x: 25, y: 30, label: 2, fill: 'orange'},
+                  {x: 35, y: 65, label: 3, fill: 'gold'},
+                  {x: 45, y: 50, label: 4, fill: 'blue'},
+                  {x: 55, y: 40, label: 5, fill: 'cyan'},
+                  {x: 65, y: 30, label: 6, fill: 'green'},
+                ]}
+              />
+              <VictoryScatter
+                style={{data: {fill: 'black'}}}
+                data={[
+                  {x: 15, y: 20},
+                  {x: 25, y: 30},
+                  {x: 35, y: 65},
+                  {x: 45, y: 50},
+                  {x: 55, y: 40},
+                  {x: 65, y: 30},
+                ]}
+              />
+            </VictoryChart>
+          </View>
+          <View style={{alignItems: 'center'}}>
+            <VictoryPie
+              colorScale="qualitative"
+              width={400}
+              height={400}
+              innerRadius={50}
+              labelRadius={120}
+              style={{labels: {fontSize: 30}}}
+              data={this.state.randomData}
+              animate={{duration: 1500}}
+            />
+          </View>
+
+          <View style={{alignItems: 'center'}}>
+            <VictoryChart width={500}>
+              <VictoryScatter
+                style={{
+                  tickValues: {fontSize: 30},
+                  labels: {fontSize: 30},
+                }}
+                data={[
+                  {
+                    x: 1,
+                    y: 3,
+                    fill: 'red',
+                    symbol: 'plus',
+                    size: 6,
+                    label: 'Red',
+                  },
+                  {
+                    x: 2,
+                    y: 5,
+                    fill: 'magenta',
+                    size: 9,
+                    opacity: 0.4,
+                    label: 'Magenta',
+                  },
+                  {
+                    x: 3,
+                    y: 4,
+                    fill: 'orange',
+                    size: 5,
+                    label: 'Orange',
+                  },
+                  {
+                    x: 4,
+                    y: 2,
+                    fill: 'brown',
+                    symbol: 'square',
+                    size: 6,
+                    label: 'Brown',
+                  },
+                  {
+                    x: 5,
+                    y: 5,
+                    fill: 'black',
+                    symbol: 'triangleUp',
+                    size: 5,
+                    label: 'Black',
+                  },
+                ]}
+              />
+            </VictoryChart>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+module.exports = VictoryDemo;
