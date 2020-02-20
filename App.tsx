b4893c18d2d0448a96bbd28b679d4a90af3be801
Chris_Reams <chrisreams91@gmail.com>
2020-02-19 21:28:42 -0600

    added lottie animations

--- App.tsx
+++ App.tsx
@@ -3,12 +3,24 @@ import React from 'react';
 import TVTabBar, { Tab } from './src/components/TVTabBar';
 import Home from './src/Home/Home';
 import GeoChart from './src/GeoChart/GeoChart';
+import LottieView from 'lottie-react-native';
 
 const tabs: Tab[] = [
   {
     key: 'Home',
     name: 'Home',
-    value: <Home />,
+    value: (
+      <LottieView
+        source={require('./hourglass.json')}
+        autoPlay
+        loop
+        style={{
+          width: 400,
+          height: 400,
+          // backgroundColor: 'blue',
+        }}
+      />
+    ),
   },
   {
     key: 'GeoChart',
@@ -18,7 +30,7 @@ const tabs: Tab[] = [
 ];
 
 const App = () => {
-  return <TVTabBar tabs={tabs} defaultTabKey="GeoChart" />;
+  return <TVTabBar tabs={tabs} defaultTabKey="Home" />;
 };
 
 export default App;
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-add replace s.platforms with "s.platforms = { :ios => "8.0", :tvos => "9.2" }"
-in RNCharts.podspec in order to build
+In order to build you will need to manually edit some podspec files from within node modules.
+
+RNCharts.podspec
+lottie-react-native.podspec
+
+In these podfiles change the s.platforms to "s.platforms = { :ios => "8.0", :tvos => "9.2" }"
 
 builds from commandline with Xcode 11.2 CLI tools
